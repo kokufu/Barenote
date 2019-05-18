b204de7ca69667df5a8cf36ff0fd8f04f8d16a3a
Yusuke Miura <kokufu.ym@gmail.com>
2019-05-17 23:44:37 -0400

    Divide webpack config to production and development

--- webpack.common.js
+++ webpack.common.js
@@ -1,9 +1,7 @@
 module.exports = {
-    mode: 'development',
     entry: './src/barenote.js',
     output: {
         path: `${__dirname}/dist`,
-        filename: 'barenote.js',
         library: 'Barenote',
         libraryExport: 'default',
         libraryTarget: 'var',
