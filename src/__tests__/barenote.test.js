b0389d0257e893185d9f848a9fd2ebff4c951b96
Yusuke Miura <kokufu.ym@gmail.com>
2019-05-29 17:21:46 -0400

    Add a test for constructor

--- src/__tests__/barenote.test.js
+++ src/__tests__/barenote.test.js
@@ -4,15 +4,26 @@ const fs = require('fs');
 const path = require('path');
 import Barenote from '../barenote'
 
-test('Constructor replaces text to number', () => {
+beforeEach(() => {
     const html = fs.readFileSync(path.resolve(__dirname, 'index.html'), {encoding: 'utf-8'});
     document.write(html);
+})
+
+test('Constructor replaces text to number', () => {
     const body = document.querySelector('body');
     new Barenote(body);
-
-    const allElements = body.querySelectorAll('.barenote');
-    expect(allElements.length).toBe(2);
-    for (const [index, el] of allElements.entries()) {
+    const allBarenotes = body.querySelectorAll('.barenote');
+    expect(allBarenotes.length).toBe(2);
+    for (const [index, el] of allBarenotes.entries()) {
         expect(el.innerText).toEqual((index + 1).toString(10));
     }
-});
+})
+
+test('Constructor makes footnote lists', () => {
+    const body = document.querySelector('body');
+    const allBarenotes = body.querySelectorAll('.barenote');
+    new Barenote(body);
+    for (const [index, el] of body.querySelectorAll('.barenote_ref_list ol li').entries()) {
+        expect(el.innerText).toEqual(allBarenotes[index].innerText)
+    }
+})
