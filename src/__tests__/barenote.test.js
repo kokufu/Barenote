35961e3ebd3d2ad1de95c6bd8a5f89d8e6151ecd
Yusuke Miura <kokufu.ym@gmail.com>
2019-05-30 17:34:42 -0400

    Add a test for multi articles

--- src/__tests__/barenote.test.js
+++ src/__tests__/barenote.test.js
@@ -4,26 +4,57 @@ const fs = require('fs');
 const path = require('path');
 import Barenote from '../barenote'
 
-beforeEach(() => {
-    const html = fs.readFileSync(path.resolve(__dirname, 'index.html'), {encoding: 'utf-8'});
-    document.write(html);
+describe("Single article", () => {
+    beforeEach(() => {
+        const html = fs.readFileSync(path.resolve(__dirname, 'index.html'), {encoding: 'utf-8'});
+        document.write(html);
+    })
+    
+    test('Constructor replaces text to number', () => {
+        const body = document.querySelector('body');
+        new Barenote(body);
+        const allBarenotes = body.querySelectorAll('.barenote');
+        expect(allBarenotes.length).toBe(2);
+        for (const [index, el] of allBarenotes.entries()) {
+            expect(el.innerText).toEqual((index + 1).toString(10));
+        }
+    })
+    
+    test('Constructor makes footnote lists', () => {
+        const body = document.querySelector('body');
+        const allBarenotes = body.querySelectorAll('.barenote');
+        new Barenote(body);
+        for (const [index, el] of body.querySelectorAll('.barenote_ref_list ol li').entries()) {
+            expect(el.innerText).toEqual(allBarenotes[index].innerText)
+        }
+    })
 })
 
-test('Constructor replaces text to number', () => {
-    const body = document.querySelector('body');
-    new Barenote(body);
-    const allBarenotes = body.querySelectorAll('.barenote');
-    expect(allBarenotes.length).toBe(2);
-    for (const [index, el] of allBarenotes.entries()) {
-        expect(el.innerText).toEqual((index + 1).toString(10));
-    }
-})
+describe("Multi articles", () => {
+    beforeEach(() => {
+        const html = fs.readFileSync(path.resolve(__dirname, 'index2.html'), {encoding: 'utf-8'})
+        document.write(html)
+    })
 
-test('Constructor makes footnote lists', () => {
-    const body = document.querySelector('body');
-    const allBarenotes = body.querySelectorAll('.barenote');
-    new Barenote(body);
-    for (const [index, el] of body.querySelectorAll('.barenote_ref_list ol li').entries()) {
-        expect(el.innerText).toEqual(allBarenotes[index].innerText)
-    }
+    test('Constructor replaces text to number', () => {
+        const articles = document.querySelectorAll('.article')
+        for (const article of articles) {
+            new Barenote(article)
+            const allBarenotes = article.querySelectorAll('.barenote')
+            for (const [index, el] of allBarenotes.entries()) {
+                expect(el.innerText).toEqual((index + 1).toString(10))
+            }
+        }
+    })
+    
+    test('Constructor makes footnote lists', () => {
+        const articles = document.querySelectorAll('.article')
+        for (const article of articles) {
+            const allBarenotes = article.querySelectorAll('.barenote')
+            new Barenote(article)
+            for (const [index, el] of article.querySelectorAll('.barenote_ref_list ol li').entries()) {
+                expect(el.innerText).toEqual(allBarenotes[index].innerText)
+            }
+        }
+    })
 })
