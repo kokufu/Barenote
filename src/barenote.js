1019817a8d13cfc0896ac3678c6474c8e9dc6633
Yusuke Miura <kokufu.ym@gmail.com>
2019-11-28 14:53:49 -0500

    Separate floating_note to another module

--- src/barenote.js
+++ src/barenote.js
@@ -1,4 +1,5 @@
 import CONST from "./const"
+import floatingNote from './floating_note'
 
 // TODO Move to another file
 const ABOUT_DIALOG = '<html><head><title>About</title></head>' +
@@ -44,78 +45,7 @@ function scrollToOwnTop() {
   });
 }
 
-const FloatingNote = function() {
-  let instance = document.querySelector(CONST.FLOATING_NOTE_SELECTOR);
-  if (instance) {
-    return instance;
-  }
-
-  instance = document.createElement('div');
-  instance.setAttribute('id', CONST.FLOATING_NOTE_ID);
-  instance.style.cssText = 'position: absolute !important;';
-  instance.hidden = true;
-  document.body.appendChild(instance);
-
-  const originalOpacity = (instance.style.opacity) ? instance.style.opacity : 1.0;
-  instance.show = function(event) {
-    const rect = event.data.element.getBoundingClientRect();
-    let left = rect.left + rect.width + window.pageXOffset;
-    const top = rect.top + rect.height + window.pageYOffset;
-
-    // TODO: measure actual instance size instead of 420px
-    if (left + 420 > window.innerWidth) {
-        left = window.innerWidth - 420;
-    }
-
-    instance.isAnimating = false;
-
-    instance.innerHTML = event.data.text;
-
-    instance.style.left = `${left}px`;
-    instance.style.top = `${top}px`;
-    instance.style.opacity = originalOpacity;
-    instance.hidden = false;
-  };
-
-  instance.hide = function() {
-    const delay = 500; // 0.5 sec
-    const start = window.performance.now();
-    const duration = 1000; // 1 sec
-
-    instance.isAnimating = true;
-    window.requestAnimationFrame(function fadeOut(now) {
-      if (!instance.isAnimating) {
-        return;
-      }
-      const progress = now - start - delay;
-      if (progress < 0) {
-        window.requestAnimationFrame(fadeOut);
-        return;
-      }
-      let opacity = originalOpacity * (1 - progress / duration);
-      opacity = (opacity < 0) ? 0 : opacity;
-      instance.style.opacity = opacity;
-
-      if (progress < duration) {
-        window.requestAnimationFrame(fadeOut);
-      } else {
-        instance.hidden = true;
-        instance.isAnimating = false;
-      }
-    });
-  };
-  return instance;
-}
-
 let objectNumber = 0;
-let floatingNote = null;
-function initFloatingNote() {
-  if (floatingNote !== null) {
-    return;
-  }
-  floatingNote = new FloatingNote();
-}
-
 class Note {
   constructor(parent, index, originalElement) {
     this.index = index;
@@ -172,7 +102,6 @@ class Note {
 export default class Barenote {
   constructor(rootElement) {
     initStyle();
-    initFloatingNote();
 
     this._notes = [];
     this._rootElement = rootElement;
