d2a55c7dd90a09b775b9f22f1410efac7bd09914
Yusuke Miura <kokufu.ym@gmail.com>
2019-11-28 19:31:15 -0500

    Install webpack-dev-server package and related

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,9 +1,22 @@
 const merge = require('webpack-merge')
 const common = require('./webpack.common')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const CopyPlugin = require('copy-webpack-plugin')
 
 module.exports = merge(common, {
     mode: 'development',
     output: {
         filename: 'barenote.js',
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: 'src/__dev__/index.html'
+        }),
+        new CopyPlugin([
+            { context: 'src/__dev__', from: '**/*.css' }
+        ]),
+    ],
+    devServer: {
+        contentBase: './dist'
     }
 })
